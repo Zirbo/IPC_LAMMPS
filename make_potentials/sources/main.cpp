b62c416ed42c4aeef5772b80cec8452252b39913
Zirbo <silvano.ferrari@ymail.com>
2021-11-21 17:53:07 +0100

    epsilon to potentials: - easier bash script - removed python script, everything is done from bash/c++

--- make_potentials/sources/main.cpp
+++ make_potentials/sources/main.cpp
@@ -1,44 +1,52 @@
 #include <unistd.h>
-#include <iostream>
+
 #include <cstring>
+#include <iostream>
 
 #include "printPotential.hpp"
 
 static void usage() {
-    std::cerr << "Usage:\n\t-j for janus instead of normal IPCs\n"
-        << "\t-i input file\n\t-o output dir\n";
+  std::cerr << "Usage:\n"
+            << "\t-m model (janus, ipc, asym-ipc)\n"
+            << "\t-i inputfile\n"
+            << "\t-o output dir\n";
+  exit(EXIT_FAILURE);
 }
 
-int main( int argc, char *argv[] ) {
-    IpcType type = IpcType::IPC;
-    std::string inputFileName;
-    std::string outputDirName;
+int main(int argc, char* argv[]) {
+  IpcType type = IpcType::NONE;
+  std::string inputFileName;
+  std::string outputDirName;
 
-    int opt = 0;
-    while ((opt = getopt(argc, argv, "ji:o:")) != -1) {
-        switch(opt) {
-        case 'j':
-            type = IpcType::JANUS;
-            break;
-        case 'i':
-            inputFileName = optarg;
-            break;
-        case 'o':
-            outputDirName = optarg;
-            break;
-        default:
-            usage();
-            return EXIT_FAILURE;
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "m:i:o:")) != -1) {
+    switch (opt) {
+      case 'm':
+        if (strcmp(optarg, "janus") == 0) {
+          type = IpcType::JANUS;
+        } else if (strcmp(optarg, "ipc") == 0) {
+          type = IpcType::IPC;
+        } else if (strcmp(optarg, "asym-ipc") == 0) {
+          type = IpcType::ASYM_IPC;
         }
-    }
-
-    if (inputFileName.empty() || outputDirName.empty()) {
+        break;
+      case 'i':
+        inputFileName = optarg;
+        break;
+      case 'o':
+        outputDirName = optarg;
+        break;
+      default:
         usage();
-        return EXIT_FAILURE;
     }
+  }
 
-    PotentialForLammps potential(inputFileName, type);
-    potential.printLAMMPSpotentialsToFile(outputDirName);
-    potential.printRadialPotentialsToFile(outputDirName);
-    potential.printAngularPotentialsToFile(outputDirName);
+  if (inputFileName.empty() || outputDirName.empty() || type == IpcType::NONE) {
+    usage();
+  }
+
+  PotentialForLammps potential(inputFileName, type);
+  potential.printLAMMPSpotentialsToFile(outputDirName);
+  potential.printRadialPotentialsToFile(outputDirName);
+  potential.printAngularPotentialsToFile(outputDirName);
 }
--- make_potentials/sources/printPotential.hpp
+++ make_potentials/sources/printPotential.hpp
@@ -1,33 +1,36 @@
-#include <fstream>
-#include <vector>
-#include <string>
 #include <cmath>
+#include <fstream>
 #include <iomanip>
+#include <string>
+#include <vector>
 
 enum class IpcType {
-    JANUS,
-    IPC,
+  NONE,
+  JANUS,
+  IPC,
+  ASYM_IPC,
 };
 
 class PotentialForLammps {
-public:
-    PotentialForLammps(std::string const& inputFileName, const IpcType ipcType);
-    void printLAMMPSpotentialsToFile(std::string const& outputDirName);
-    void printRadialPotentialsToFile(std::string const& outputDirName);
-    void printAngularPotentialsToFile(std::string const& outputDirName);
-private:
-    IpcType ipcType;
-    double e_BB, e_Bs1, e_Bs2, e_s1s1, e_s1s2, e_s2s2, e_min;
-    double firstPatchEccentricity, firstPatchRadius;
-    double secndPatchEccentricity, secndPatchRadius;
-    double ipcRadius, interactionRange;
-    double fakeHSdiameter, fakeHScoefficient, fakeHSexponent;
+ public:
+  PotentialForLammps(std::string const &inputFileName, const IpcType ipcType);
+  void printLAMMPSpotentialsToFile(std::string const &outputDirName);
+  void printRadialPotentialsToFile(std::string const &outputDirName);
+  void printAngularPotentialsToFile(std::string const &outputDirName);
 
-    double samplingStep, cutoffValue;
+ private:
+  IpcType ipcType;
+  double e_BB, e_Bs1, e_Bs2, e_s1s1, e_s1s2, e_s2s2, e_min;
+  double eccentricity_p1, radius_p1;
+  double eccentricity_p2, radius_p2;
+  double ipcRadius, interactionRange;
+  double fakeHSdiameter, fakeHScoefficient, fakeHSexponent;
 
-    std::vector<double> uHS, uBB, uBs1, uBs2, us1s2, us1s1, us2s2;
-    std::vector<double> fHS, fBB, fBs1, fBs2, fs1s2, fs1s1, fs2s2;
+  double samplingStep, cutoffValue;
 
-    void computeSiteSitePotentials();
-    size_t dist(double x, double y);
+  std::vector<double> uHS, uBB, uBs1, uBs2, us1s2, us1s1, us2s2;
+  std::vector<double> fHS, fBB, fBs1, fBs2, fs1s2, fs1s1, fs2s2;
+
+  void computeSiteSitePotentials();
+  size_t dist(double x, double y);
 };
